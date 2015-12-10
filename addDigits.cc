73e125409bed20616e66b72241d6fdf5a0004cda
韩培义 <hanker@hanpeiyis-MacBook-Pro.local>
2015-12-10 20:54:48 +0800

    add addDigits

---
+++ addDigits.cc
@@ -0,0 +1,44 @@
+// Source : https://leetcode.com/problems/add-digits/
+// Author : hanker
+// Date   : 2015-12-10
+
+/********************************************************************************** 
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ * For example:
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *
+ * Follow up:
+ * 	Could you do it without any loop/recursion in O(1) runtime?
+ *
+ **********************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std; 
+
+class Solution {
+public:
+	int addDigits(int num) {
+        	if ((num/10) == 0)
+			return num;
+		else {
+			return addDigits(getDigit(num));
+		}
+    	}
+    	// easy method
+    	int addDigits02(int num) {
+    		return (num-1)%9 + 1;
+	}
+
+    	int getDigit(int num) {
+		int digit = 0;
+		while(num) {
+			digit += (num%10);
+			num /= 10;
+		}	
+		return digit;
+   	}
+};
+
